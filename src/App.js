7edfb6fa019c8152e3701c7ed5aa9b81e0c00ecc
HASAN <148566101+HASANMAHMUD25@users.noreply.github.com>
2023-11-21 17:01:50 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import './App.css';
+import Home from "./pages/Home";
+import Layout from "./layouts/Layouts";
+import { useState } from "react";
+import DataMovie from '../src/Utils/components/DataMovie'
+import Add from "../src/pages/Add"
+import GlobalStyle from "./components/Global/GlobalStyle";
+import Popular from "./pages/Popular";
+
+
+function App() {
+
+  const [ data, setData ] = useState(DataMovie)
+  return (
+    <>
+    <GlobalStyle />
+    <Layout>
+        <Routes>
+          <Route path='/' element={<Home item={data} setItem={setData}/>} />
+          <Route path='/popular' element={<Popular/>} />
+          <Route path='/Add' element={<Add item={data} setItem={setData} />} />
+        </Routes>
+    </Layout>
+    </>
+  );
+}
+
+export default App;
